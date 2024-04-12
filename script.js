f1ae48c14dee1e913efdd16eba0067aec9cac909
alex-penguin <apenman1991@gmail.com>
2024-04-13 00:02:35 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 // Define an object containing translations for each key in different languages
 const translations = {
     'en': {
-        'post1-title': 'Blog Post Title',
-        'post1-content': 'This is a sample blog post content in English.'
+        'post1-title': 'Hello and welcome to alexpenman.com.au !!',
+        'post1-content': 'This is still a work in progress, but feel free to look around!'
     },
     'es': {
-        'post1-title': 'Título del Post del Blog',
-        'post1-content': 'Este es un ejemplo de contenido de un post del blog en Español.'
+        'post1-title': 'Hola y bienvenido a alexpenman.com.au!!',
+        'post1-content': 'Hola y bienvenido a alexpenman.com.au!!.'
+        
     },
-    // Add more languages here...
-};
+    'fr': { 
+        'post1-title': 'Bonjour et bienvue sur alexpenman.com.au !!'
+        'post1-content': 'Bonjour et bienvenue sur alexpenman.com.au !!'            
+    
+    },
+    'de': {
+        'post1-title': 'Hallo und willkommen bei alexpenman.com.au !!',
+        'post1-content': 'Hallo und willkommen bei alexpenman.com.au !!'
+
+    },
+    'it': {
+        'post1-title': 'Ciao e benvenuto su alexpenman.com.au !!',
+        'post1-content': 'Ciao e benvenuto su alexpenman.com.au !!'
+    
+    },
+    'pt': {         
+        'post1-title': 'Olá e bem-vindo ao alexpenman.com.au !!',
+        'post1-content': 'Olá e bem-vindo ao alexpenman.com.au !!'
+    
+    },
+    
+    ''
+
+
+
+
 
 // Function to change the language of the page
 function changeLanguage(lang) {
